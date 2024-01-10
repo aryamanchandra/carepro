1106e1d38fa933c04d5b53b56f53b6277f6fe981
aryamanchandra <aryamanhere@gmail.com>
2024-01-10 15:28:00 +0530

    mui setup

---
+++ style.ts
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#E8E8E8", //light gray
+      light: "#183153", //accent
+    },
+    secondary: {
+      main: "#0F0F0F", //dark
+      light: "#F2F9FF", //secondary accent
+      dark: "#41557A", //tertiary accent
+    },
+    info: {
+      main: "#666666", //normal text
+      light: "#B0B0B0", //light text
+    },
+    error: {
+      main: "#BB0A21", //red
+    },
+    success: {
+      main: "#007D55", //green
+    },
+  },
+});
+
+export {theme} 
