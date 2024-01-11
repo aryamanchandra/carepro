e94faaf6c28ff7a14db2db3dad46b82785b2d356
aryamanchandra <aryamanhere@gmail.com>
2024-01-11 15:34:29 +0530

    update patient table and patient page styling

--- src/app/Patients/page.js
+++ src/app/Patients/page.js
@@ -3,6 +3,7 @@ import * as React from "react";
 import {
   Box,
   Button,
+  InputAdornment,
   Stack,
   Tab,
   Tabs,
@@ -11,6 +12,9 @@ import {
 } from "@mui/material";
 import IPDTable from "../components/Table/IPDTable";
 import NotificationsIcon from "@mui/icons-material/Notifications";
+import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from "@mui/icons-material/Search";
+import PatientTable from "../components/Table/PatientTable";
 
 export default function Patients() {
   return (
@@ -29,24 +33,60 @@ export default function Patients() {
       </Stack>
       <Box
         sx={{
-          width: "100%",
+          width: "95%",
           backgroundColor: "#fff",
           padding: "20px",
           paddingX: "40px",
           borderRadius: "10px",
         }}
       >
-        <Typography>Patient Info</Typography>
+        <Stack
+          direction="row"
+          sx={{
+            paddingTop: "10px",
+            paddingBottom: "20px",
+            borderBottom: "1px solid #B0B0B0",
+          }}
+          alignItems="center"
+        >
+          <Typography>Patient Info</Typography>
+          <Button
+            variant="contained"
+            startIcon={<AddIcon />}
+            sx={{ marginLeft: "auto" }}
+          >
+            New Patient
+          </Button>
+        </Stack>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <Stack direction="row">
-            <TextField id="filled-basic" label="Filled" variant="filled" />
+          <Stack direction="row" sx={{ marginY: "20px" }}>
+            {/* <TextField
+              id="filled-basic"
+              label="Search"
+              variant="filled"
+              startIcon={<SearchIcon />}
+              sx={{ borderRadius: "20px", marginRight: "20px" }}
+            /> */}
+            <TextField
+              id="input-with-icon-textfield"
+              placeholder="Search"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+              variant="filled"
+              sx={{ borderRadius: "20px !important", marginRight: "20px" }}
+            />
             <TextField
               id="outlined-basic"
               label="Outlined"
               variant="outlined"
             />
           </Stack>
-          <IPDTable />
+          <PatientTable />
         </Box>
       </Box>
     </Box>
