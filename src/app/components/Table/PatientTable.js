75c88f1602198b9a85212aa6bb4c7f81aec62fb1
aryamanchandra <aryamanhere@gmail.com>
2024-01-19 13:04:41 +0530

    table styling update

--- src/app/components/Table/PatientTable.js
+++ src/app/components/Table/PatientTable.js
@@ -7,25 +7,70 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Button } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
-import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import CloseIcon from "@mui/icons-material/Close";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, calories, fat, carbs, protein, email) {
+  return { name, calories, fat, carbs, protein, email };
 }
 
 const rows = [
-  createData("9:30 AM", "Elizabeth Polson", "05/12/2022", "Doctor John"),
-  createData("9:30 AM", "John David", "05/12/2022", "Doctor Joel"),
-  createData("10:00 AM", "Krishtav Rajan", "05/12/2022", "Doctor Joel"),
-  createData("11:30 AM", "Sumanth Tinson", "05/12/2022", "Doctor John"),
-  createData("930 AM", "EG Subtornani", "05/12/2022", "Doctor John"),
-  createData("11:00 AM", "John David", "05/12/2022", "Doctor Joel"),
-  createData("11:30 AM", "Krishtov Rojan", "05/12/2022", "Doctor Joel"),
-  createData("11:00 AM", "Sumanth Tinson", "05/12/2022", "Doctor John"),
-  createData("11:00 AM", "EG Subramani", "05/12/2022", "Doctor John"),
-  createData("11:00 AM", "Sumanth rinson", "05/12/2022", "Doctor John"),
-  createData("11:00 AM", "EG Subrornoni", "05/12/2022", "Doctor Joel"),
+  createData(
+    "Elizabeth Polson",
+    32,
+    "Female",
+    "B+ve",
+    "49112345 67890",
+    "elsobethpolsan@hotmail.com"
+  ),
+  createData(
+    "John David",
+    24,
+    "Male",
+    "AB+ve",
+    "+9112345 67890",
+    "davidj0hn22@gmail.com"
+  ),
+  createData(
+    "Krishtav Rajan",
+    77,
+    "Male",
+    "O+ve",
+    "+9112345 67890",
+    "krishnarajon23@grnailcom"
+  ),
+  createData(
+    "Sumanth Tinson",
+    77,
+    "Male",
+    "AB+ve",
+    "12345 67890",
+    "tintintin@gmailcom"
+  ),
+  createData(
+    "EG Subramani",
+    55,
+    "Male",
+    "O-ve",
+    "+9112345 67890",
+    "egs31322@gmail.com"
+  ),
+  createData(
+    "Ronjan Moori",
+    55,
+    "Male",
+    "O+ve",
+    "+9112345 67890",
+    "ranajonmaari@yahoo.com"
+  ),
+  createData(
+    "Philipile Gopal",
+    55,
+    "Male",
+    "O-ve",
+    "+9112345 67890",
+    "gop0122@gmoil.com"
+  ),
 ];
 
 export default function BasicTable() {
@@ -40,17 +85,20 @@ export default function BasicTable() {
       >
         <TableHead>
           <TableRow>
-            <TableCell>Time</TableCell>
             <TableCell>Patient Name</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Doctor</TableCell>
+            <TableCell>Age</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Blood Group</TableCell>
+            <TableCell>Phone Number</TableCell>
+            <TableCell>Email ID</TableCell>
+            <TableCell>User Action</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
               key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0} }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {row.name}
@@ -59,8 +107,15 @@ export default function BasicTable() {
               <TableCell>{row.fat}</TableCell>
               <TableCell>{row.carbs}</TableCell>
               <TableCell>{row.protein}</TableCell>
-              <TableCell><Button><CloseIcon /></Button></TableCell>
-              <TableCell><Button><QuestionMarkIcon /></Button></TableCell>
+              <TableCell>{row.email}</TableCell>
+              <TableCell>
+                <Button sx={{border:"#FF6558 solid 1px", color:"#FF6558", minWidth:"20px", marginRight:"10px"}}>
+                  <CloseIcon />
+                </Button>
+                <Button sx={{border:"#3497F9 solid 1px", color:"#3497F9", minWidth:"20px"}}>
+                  <QuestionMarkIcon />
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
