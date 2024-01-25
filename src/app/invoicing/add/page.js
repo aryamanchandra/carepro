505e33f15cac70f6a95bb287556867ed9f30f95e
aryamanchandra <aryamanhere@gmail.com>
2024-01-25 07:48:46 +0530

    invoice form update

--- src/app/invoicing/add/page.js
+++ src/app/invoicing/add/page.js
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   Divider,
+  Grid,
   Stack,
   Tab,
   Tabs,
@@ -12,41 +13,8 @@ import NotificationsIcon from "@mui/icons-material/Notifications";
 import Link from "next/link";
 import { useState } from "react";
 import PropTypes from "prop-types";
-import RegistrationForm from "@/app/components/Form/RegistrationForm";
+import InvoiceForm from "@/app/components/Form/InvoiceForm";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-
-function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-CustomTabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
 
 export default function Add() {
   const [value, setValue] = useState(0);
@@ -58,7 +26,7 @@ export default function Add() {
     <Box sx={{ backgroundColor: "#F1F8FF", padding: "20px" }}>
       <Stack direction="row" marginBottom="10px">
         <Typography variant="h6" color="primary.light">
-          IPD Patient Details
+          Invoicing
         </Typography>
         <Button startIcon={<NotificationsIcon />} sx={{ marginLeft: "auto" }} />
         <Stack direction="column">
@@ -73,7 +41,7 @@ export default function Add() {
           sx={{ marginBottom: "20px", color: "#3497F9" }}
           startIcon={<ArrowBackIosIcon />}
         >
-          Go Back to Patients
+          Go Back to Invoices
         </Button>
       </Link>
       <Box
@@ -84,12 +52,26 @@ export default function Add() {
           alignItems="center"
           sx={{ marginBottom: "20px" }}
         >
-          <Typography variant="subtitle1"> PATIENT NO. 10214234534</Typography>
+          <Typography variant="subtitle1">INVOICE NO: #299533</Typography>
           <Button
             variant="contained"
             sx={{
               paddingY: "10px",
               marginLeft: "auto",
+              backgroundColor: "#fff",
+              color:"#3497F9",
+              boxShadow: "none",
+              py: 1,
+              px:3,
+              borderRadius: "5px",
+            }}
+          >
+            Print
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              paddingY: "10px",
               backgroundColor: "#3497F9",
               boxShadow: "none",
               py: 1,
@@ -101,29 +83,14 @@ export default function Add() {
           </Button>
         </Stack>
         <Divider />
-        <Box
-          sx={{ borderBottom: 1, borderColor: "divider", marginTop: "10px" }}
-        >
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-            textColor="#3497F9"
-          >
-            <Tab label="Registration Form" {...a11yProps(0)} />
-            <Tab label="Risk Coverage Declaration" {...a11yProps(1)} />
-            <Tab label="Documents" {...a11yProps(2)} />
-          </Tabs>
-        </Box>
-        <CustomTabPanel value={value} index={0}>
-          <RegistrationForm />
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={1}>
-          Item Two
-        </CustomTabPanel>
-        <CustomTabPanel value={value} index={2}>
-          Item Three
-        </CustomTabPanel>
+        <Grid container>
+          <Grid item xs={8}>
+            <InvoiceForm />
+          </Grid>
+          <Grid item xs={4}>
+            <Typography>Hello</Typography>
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
